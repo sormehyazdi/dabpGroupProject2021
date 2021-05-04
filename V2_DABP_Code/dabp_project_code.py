29f48120aeccf6523f35d19c9a4721671a5fc487
sormehyazdi <47797545+sormehyazdi@users.noreply.github.com>
2021-05-03 23:45:46 -0700

    now prints a dataframe with budget, cost and total distance for the optimal solution for scenario 1, model 1. Still working on the rest

--- V2_DABP_Code/dabp_project_code.py
+++ V2_DABP_Code/dabp_project_code.py
@@ -1,6 +1,6 @@
 """
 File Name: dabp_project_code.py
-Author: aliiftik, ppuvvadi, jscanlo2, sormehy
+Authors: aliiftik, ppuvvadi, jscanlo2, sormehy
 Date: May, 2021
 Python version: 3
 Purpose: POD site allocation 
@@ -9,6 +9,8 @@ Purpose: POD site allocation
     - hours each POD is available = 12
     - people per vehicle = 3
     - each POD can serve 7 cars per hour
+    - 2-3 different scenarios
+    - 2 different models for each scenario
 """
 
 ## Import required packages
@@ -33,6 +35,7 @@ def define_parameters():
     hoursOpen = 12
     peoplePerCar = 3
     carsPerHourPerLoading = 6
+    max_days_open = 30
 
     #distance = pd.read_csv('Distances.csv', header=None, sep=',')
     distance = genfromtxt(path + '/Distances.csv', delimiter=',') 
@@ -52,9 +55,9 @@ def define_parameters():
     labor = genfromtxt(path + '/Labor.csv', delimiter=',') 
     cost = labor*12 + (supplies.sum()*loadingSites*hoursOpen*carsPerHourPerLoading)
 
-    return(pod_sites, blocks, distance, population, loadingSites, capacity, supplies, labor, cost)
+    return(pod_sites, blocks, distance, population, loadingSites, capacity, supplies, labor, cost, max_days_open)
 
-def min_tot_distance(pod_sites, blocks, distance, population, loadingSites, capacity, supplies, labor, cost, budget, opening_cost):
+def min_tot_distance(pod_sites, blocks, distance, population, loadingSites, capacity, supplies, labor, cost, budget, opening_cost, bigM, max_days_open):
     ####### MODEL 1
     m1 = gp.Model()
 
@@ -74,8 +77,8 @@ def min_tot_distance(pod_sites, blocks, distance, population, loadingSites, capa
     ## all pod sites are bounded by a given capacity
     for i in pod_sites:
         m1.addConstr(sum(y1[i, k]*population[k] for k in blocks) <= capacity[i]*x1[i])
-        m1.addConstr(z1[i]*bob >= x1[i])
-        m1.addConstr(x1[i] <= 30)
+        m1.addConstr(z1[i]*bigM >= x1[i])
+        m1.addConstr(x1[i] <= max_days_open)
      
     ## each block is assigned to exactly 1 pod site
     for k in blocks:
@@ -94,7 +97,7 @@ def min_tot_distance(pod_sites, blocks, distance, population, loadingSites, capa
         for k in blocks:
             if(x1[i].x >= 0):
                 lst_np[i,k] = distance[k,i]*y1[i,k].x
-                lst.append(distance[k,i]*y1[i,k].x)
+                lst.append((i, k, distance[k,i]*y1[i,k].x))
     
     # Print optimized solution
     print(m1.objVal)
@@ -106,34 +109,40 @@ def run_scenario_one():
 
     ## Setting up some parameters that will be looped through the main function
     opening_cost = [5000, 10000, 25000, 50000, 75000, 100000]
-    budget = [100000, 250000, 500000, 1000000, 1500000, 2000000]
-    bob = 1000000000000 ## This is our big M
+    #budget = [1000000000, 2500000, 5000000, 10000000, 15000000, 20000000]
+    budget = [500000000, 1000000000]
+    bigM = 100000000000000000000 ## This is our big M
 
     ## Define Parameters
-    (pod_sites, blocks, distance, population, loadingSites, capacity, supplies, labor, cost) = define_parameters()
-    print("Set up parameters...")
+    (pod_sites, blocks, distance, population, loadingSites, capacity, supplies, labor, cost, max_days_open) = define_parameters()
+    print("*** Set up parameters...")
 
     scn1_model1_optimal_vals = []
-
-
-    ## Loop through budget values to 
+    good_options = []
+    bad_options = []
+    ## Loop through budget values, then opening cost to find optimal solutions 
     for b in budget:
-        try:
-            for op_co in opening_cost:
+        for op_co in opening_cost:
+            try:
                 (m1_opt_solution, lst) = min_tot_distance(pod_sites, blocks, distance, 
-                    population, loadingSites, capacity, supplies, labor, cost, b, op_co)
-            scn1_model1_optimal_vals.append(m1_opt_solution)
+                    population, loadingSites, capacity, supplies, labor, cost, b, op_co, bigM, max_days_open)
+                good_options.append((m1_opt_solution, lst))
 
-        except AttributeError:
-            options.append(b)
-            print("Budget", b, "with opening cost", op_co, "was infeasible...moving on...")
-            continue
+            ## First we make a list of the budget, opening cost, and optimal solution for that pair
+                scn1_model1_optimal_vals.append((b, op_co, m1_opt_solution))
 
-    return(scn1_model1_optimal_vals)
+            except AttributeError:
+                bad_options.append(b)
+                print("*** Budget", b, "with opening cost", op_co, "was infeasible...moving on...")
+                continue
+
+    scn1_m1_optimal_vals_df = pd.DataFrame(scn1_model1_optimal_vals, columns = ['Budget', 'Cost', 'TotalTravel(mi)'])
+
+    return(scn1_m1_optimal_vals_df)
 
 def main():
     optimal_vals_1 = run_scenario_one()
-    print(optimal_vals_1)
+    print("*** Scenario 1 - Model 1: \n", optimal_vals_1)
     
 
 if __name__ == '__main__':
