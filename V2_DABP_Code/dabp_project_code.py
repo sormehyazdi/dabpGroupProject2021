780e6eeb5d784834e3346977e4fa8c0bf566fce9
sormehyazdi <47797545+sormehyazdi@users.noreply.github.com>
2021-05-03 22:14:36 -0700

    rewriting everything into functions and trying to generally clean code

--- V2_DABP_Code/dabp_project_code.py
+++ V2_DABP_Code/dabp_project_code.py
@@ -11,6 +11,7 @@ Purpose: POD site allocation
     - each POD can serve 7 cars per hour
 """
 
+## Import required packages
 import gurobipy as gp
 from gurobipy import GRB
 import numpy as np
@@ -18,7 +19,7 @@ import pandas as pd
 import os
 from numpy import genfromtxt
 
-
+## Global variable
 path = os.getcwd()
 
 def define_parameters():
@@ -40,7 +41,6 @@ def define_parameters():
     population = genfromtxt(path + '/Populations.csv', delimiter=',') 
     #population = population.astype(float)
 
-
     #loadingSites = pd.read_csv('LoadingSites.csv', header=None, sep=',')
     loadingSites = genfromtxt(path + '/LoadingSites.csv', delimiter=',') 
     capacity = loadingSites * hoursOpen * peoplePerCar * carsPerHourPerLoading
@@ -52,44 +52,41 @@ def define_parameters():
     labor = genfromtxt(path + '/Labor.csv', delimiter=',') 
     cost = labor*12 + (supplies.sum()*loadingSites*hoursOpen*carsPerHourPerLoading)
 
-    budget = [1000000000, 2000000000, 10000000000, 100000000000, 1000000000000]
+    return(pod_sites, blocks, distance, population, loadingSites, capacity, supplies, labor, cost)
 
-    return(pod_sites, blocks, distance, population, loadingSites, capacity, supplies, labor, cost, budget)
+def min_tot_distance(pod_sites, blocks, distance, population, loadingSites, capacity, supplies, labor, cost, budget, opening_cost):
+    ####### MODEL 1
+    m1 = gp.Model()
 
-def find_min_budget(pod_sites, blocks, distance, population, loadingSites, capacity, supplies, labor, cost, budget):
-####### MODEL to figure out what the minimum cost would be (assuming we fix the prices)
-    m_cost = gp.Model()
+    ### Decision variables
+    ## number of days POD is open for
+    x1 = m1.addVars(pod_sites, vtype=GRB.CONTINUOUS, lb = 0.0)
+    ## whether or not a block is assigned to a POD
+    y1 = m1.addVars(pod_sites, blocks, vtype=GRB.BINARY)
+    ## whether or not a POD is open
+    z1 = m1.addVars(pod_sites, vtype=GRB.BINARY)
 
-    # Decision variables
-    x1 = m_cost.addVars(pod_sites, vtype=GRB.CONTINUOUS, lb = 0.0)
-    y1 = m_cost.addVars(pod_sites, blocks, vtype=GRB.BINARY)
-    #days = m_cost.addVars(GRB.CONTINUOUS, lb = 0.0)
+    ### Objective function
+    ## want to minimize the total/weighted travel distance of Allegheny's population
+    m1.setObjective(sum(sum(distance[k,i]*population[k]*y1[i,k] for i in pod_sites) for k in blocks))
     
-    
-    
-    # Objective function
-    m_cost.setObjective(sum(cost[i]*x1[i] for i in pod_sites))
-    #m_cost.setObjective(sum(sum(distance[k, i]*population[k]*y1[i,k] for i in pod_sites) for k in blocks))
-    
-    
-    # Constraints
-    
+    ### Constraints
     ## all pod sites are bounded by a given capacity
     for i in pod_sites:
-        m_cost.addConstr(sum(y1[i, k]*population[k] for k in blocks) <= capacity[i]*x1[i])
-     #   m_cost.addConstr(sum(y1[i, k]*population.iloc[k] for k in blocks) <= capacity.iloc[i]*x1[i])
+        m1.addConstr(sum(y1[i, k]*population[k] for k in blocks) <= capacity[i]*x1[i])
+        m1.addConstr(z1[i]*bob >= x1[i])
+        m1.addConstr(x1[i] <= 30)
      
-    
     ## each block is assigned to exactly 1 pod site
     for k in blocks:
-        m_cost.addConstr(sum(y1[i, k] for i in pod_sites) == 1)
+        m1.addConstr(sum(y1[i, k] for i in pod_sites) == 1)
 
         # the total money used is at most the maximum budget
-        #m_cost.addConstr(sum(cost[i]*x1[i] for i in pod_sites) <= b)
-        #m_cost.addConstr(sum(cost.iloc[i]*x1[i] for i in pod_sites) <= budget)
+        m1.addConstr(sum(cost[i]*x1[i] + z1[i]*opening_cost for i in pod_sites) <= budget)
 
-    m_cost.optimize()
-    
+    ## Solve
+    m1.optimize()
+
     lst = []
     lst_np = np.zeros([len(pod_sites), 1100])
 
@@ -99,68 +96,45 @@ def find_min_budget(pod_sites, blocks, distance, population, loadingSites, capac
                 lst_np[i,k] = distance[k,i]*y1[i,k].x
                 lst.append(distance[k,i]*y1[i,k].x)
     
-    lst_6.append(lst_np)   
-    return(m_cost.objVal) 
+    # Print optimized solution
+    print(m1.objVal)
 
-"""
-def min_tot_distance(pod_sites, blocks, distance, population, loadingSites, capacity, supplies, labor, cost, budget):
-    try:
-        ####### MODEL 1
-        m1 = gp.Model()
-    
-        # Decision variables
-        x1 = m1.addVars(pod_sites, vtype=GRB.CONTINUOUS, lb = 0.0)
-        y1 = m1.addVars(pod_sites, blocks, vtype=GRB.BINARY)
-        #days = m1.addVars(GRB.CONTINUOUS, lb = 0.0)
-        
-        
-        
-        # Objective function
-        m1.setObjective(sum(sum(distance[k,i]*population[k]*y1[i,k] for i in pod_sites) for k in blocks))
-        #m1.setObjective(sum(sum(distance[k, i]*population[k]*y1[i,k] for i in pod_sites) for k in blocks))
-        
-        
-        # Constraints
-        
-        ## all pod sites are bounded by a given capacity
-        for i in pod_sites:
-            m1.addConstr(sum(y1[i, k]*population[k] for k in blocks) <= capacity[i]*x1[i])
-         #   m1.addConstr(sum(y1[i, k]*population.iloc[k] for k in blocks) <= capacity.iloc[i]*x1[i])
-         
-        
-        ## each block is assigned to exactly 1 pod site
-        for k in blocks:
-            m1.addConstr(sum(y1[i, k] for i in pod_sites) == 1)
-    
-            # the total money used is at most the maximum budget
-            m1.addConstr(sum(cost[i]*x1[i] for i in pod_sites) <= b)
-            #m1.addConstr(sum(cost.iloc[i]*x1[i] for i in pod_sites) <= budget)
-    
-        m1.optimize()
-        
-        lst = []
-        lst_np = np.zeros([len(pod_sites), 1100])
-    
-        for i in pod_sites:
-            for k in blocks:
-                if(x1[i].x >= 0):
-                    lst_np[i,k] = distance[k,i]*y1[i,k].x
-                    lst.append(distance[k,i]*y1[i,k].x)
-        
-        lst_6.append(lst_np)
-    except AttributeError:
-        options.append(b)
-        print(b, "was infeasible...moving on...")
-        continue
-"""
-def main():
-    (pod_sites, blocks, distance, population, loadingSites, capacity, supplies, labor, cost, budget) = define_parameters()
-    #min_tot_distance(pod_sites, blocks, distance, population, 
-    #    loadingSites, capacity, supplies, labor, cost, budget)
+    return(m1.objVal, lst) 
+
+def run_scenario_one():
+    ## Global epidemic in Allegheny county
+
+    ## Setting up some parameters that will be looped through the main function
+    opening_cost = [5000, 10000, 25000, 50000, 75000, 100000]
+    budget = [100000, 250000, 500000, 1000000, 1500000, 2000000]
+    bob = 1000000000000 ## This is our big M
+
+    ## Define Parameters
+    (pod_sites, blocks, distance, population, loadingSites, capacity, supplies, labor, cost) = define_parameters()
     print("Set up parameters...")
-    petra = find_min_budget(pod_sites, blocks, distance, population, loadingSites, capacity, supplies, labor, cost, budget)
-    print("Ran find_min_budget...")
-    print("PETRA", petra)
+
+    scn1_model1_optimal_vals = []
+
+
+    ## Loop through budget values to 
+    for b in budget:
+        try:
+            for op_co in opening_cost:
+                (m1_opt_solution, lst) = min_tot_distance(pod_sites, blocks, distance, 
+                    population, loadingSites, capacity, supplies, labor, cost, b, op_co)
+            scn1_model1_optimal_vals.append(m1_opt_solution)
+
+        except AttributeError:
+            options.append(b)
+            print("Budget", b, "with opening cost", op_co, "was infeasible...moving on...")
+            continue
+
+    return(scn1_model1_optimal_vals)
+
+def main():
+    optimal_vals_1 = run_scenario_one()
+    print(optimal_vals_1)
+    
 
 if __name__ == '__main__':
     main()
